3d568825f619c081f618446567356ed3ac9cee07
Micke Larsson <micke@miklar.se>
2019-07-04 17:55:56 +0200

    Update programs via homebrew.sh

--- programs via homebrew.sh
+++ programs via homebrew.sh
@@ -5,3 +5,9 @@
 #brew cask install google-chrome
 brew cask install iterm2
 brew install neovim
+
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# The installation script should set zsh to your default shell, but if it doesn't you can do it manually:
+# chsh -s $(which zsh)
