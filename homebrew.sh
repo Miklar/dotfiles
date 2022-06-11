174572265112bdccc932f3426777698df0184d9d
miklar <micke@miklar.se>
2022-06-11 17:40:36 +0200

    Adds fish as default shell

--- homebrew.sh
+++ homebrew.sh
@@ -8,6 +8,7 @@ brew install --cask iterm2
 brew install neovim
 brew install z
 brew install --cask docker
+brew install diff-so-fancy #pretty git diffs
 
 brew install nvm
 mkdir ~/.nvm
@@ -17,8 +18,15 @@ brew tap homebrew/cask-fonts #You only need to do this once for cask-fonts
 brew install --cask font-fantasque-sans-mono
 brew install --cask font-fira-code
 
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install fish
+# set fish as default shell
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+#brew install zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # The installation script should set zsh to your default shell, but if it doesn't you can do it manually:
 # chsh -s $(which zsh)
