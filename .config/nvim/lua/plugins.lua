164464e711a7dc2b20a244cb1771f647f9880e14
miklar <micke@miklar.se>
2022-06-11 18:45:00 +0200

    Add Neovim plugin config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,42 @@
+-- Update with :PackerUpdate 
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- so packer can update itself
+  use { -- nice interface for LSP functions (among other things)
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'neovim/nvim-lspconfig' -- native LSP support
+  use 'hrsh7th/nvim-cmp' -- autocompletion framework
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP autocompletion provider
+  use 'morhetz/gruvbox' -- theme
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- autocomplete config
+  local cmp = require 'cmp'
+  cmp.setup {
+    mapping = {
+      ['<Tab>'] = cmp.mapping.select_next_item(),
+      ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      })
+    },
+    sources = {
+      { name = 'nvim_lsp' },
+    }
+  }
+
+  -- omnisharp lsp config
+  require'lspconfig'.omnisharp.setup {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    on_attach = function(_, bufnr)
+      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    end,
+    cmd = { "/usr/local/opt/omnisharp-roslyn/bin/omnisharp/run", "--languageserver" , "--hostPID", tostring(pid) },
+  }
+end)
+
