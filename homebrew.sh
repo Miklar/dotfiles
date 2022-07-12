285d657b262ccb1fae888896ff6cd6575ccb6ef0
Micke Larsson <micke@miklar.se>
2022-07-12 23:14:54 +0200

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -2,6 +2,8 @@
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/miklar/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install --cask google-chrome
 brew install --cask iterm2
